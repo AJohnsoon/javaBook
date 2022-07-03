879642f951f2fbf07a2bb1303ac890e7632e091f
ajohnsoon <allanjohnson.leal@gmail.com>
2022-07-03 10:10:04 -0300

    refactor: change used names

--- exercise/src/chapter14/example.java
+++ exercise/src/chapter14/example.java
@@ -5,12 +5,12 @@ import chapter14.entities.BusinessAccount;
 
 public class example {
     public static void main(String[] args){
-        Account acc = new Account(123456, "Allan");
+        Account acc = new Account(123456, "Jose");
         acc.Deposit(600);
         acc.Withdraw(30);
         System.out.println(acc);
 
-        BusinessAccount businessAccount = new BusinessAccount(123, "Pedro", 6000.00, 1500);
+        BusinessAccount businessAccount = new BusinessAccount(123, "Antônio", 6000.00, 1500);
         businessAccount.Loan(1500);
         System.out.println(businessAccount);
     }
