e051804ea315e4e8122851da0484f371129e1596
ajohnsoon <allanjohnson.leal@gmail.com>
2022-07-27 18:15:33 -0300

    refactor: add try and catch

--- exercise/src/chapter15/application/example02.java
+++ exercise/src/chapter15/application/example02.java
@@ -7,9 +7,14 @@ import chapter15.common.entities.Reservation;
 
 public class example02 {
     public static void main(String[] args) throws ParseException{
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");        
         Reservation objectReservation = new Reservation(1325,  sdf.parse("25/11/2022"), sdf.parse("25/12/2022"));
-        objectReservation.updatedDates(sdf.parse("20/01/2022"), sdf.parse("10/12/2021"));
-        System.out.println(objectReservation);
+        try{
+            objectReservation.updatedDates(sdf.parse("20"), sdf.parse("10/12/2021"));
+        }
+        catch(ParseException e){
+            System.out.println(e);
+        }
+        System.out.println(objectReservation);    
     }
 }
